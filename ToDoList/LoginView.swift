936097a500e59cbaaac934d7c180d45e998a9c04
jessicayingg <jessicaxying@gmail.com>
2025-03-20 17:41:00 -0400

    Working on home page

--- ToDoList/LoginView.swift
+++ ToDoList/LoginView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // A vertical stack
+        VStack {
+            // Header
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(Color.pink)
+                    .rotationEffect(Angle(degrees: 15))
+                VStack {
+                    Text("To Do List")
+                        .font(.system(size: 50))
+                        .foregroundColor(Color.white)
+                        .bold()
+                    Text("Get things done")
+                        .font(.system(size: 30))
+                        .foregroundColor(Color.white)
+                }
+                .padding(.top, 30)
+            }
+            .frame(width: UIScreen.main.bounds.width * 3,
+                   height: 300)
+            // offset for negative y pushes the rectangle up
+            .offset(y: -100)
+            
+            // Login Form
+            
+            // Create Account
+            
+            Spacer()
+        }
     }
 }
 
