e7c2b0bc244894023c3f37215379979e7815306f
jessicayingg <jessicaxying@gmail.com>
2025-03-21 03:12:50 -0400

    UI for todo items + basic logic for making todo list items

--- ToDoList/NewItemViewViewModel.swift
+++ ToDoList/NewItemViewViewModel.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
     init() {}
+    
+    func save() {
+        
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        // 86400 sceonds = number of seconds in a day
+        // This helps to get rid of weird timezone problems
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
 }
