d7bbb51acf5531212e24807882fd2bcdc35fe8e2
jessicayingg <jessicaxying@gmail.com>
2025-03-22 11:41:19 -0400

    Profile view ui

--- ToDoList/ProfileView.swift
+++ ToDoList/ProfileView.swift
@@ -13,6 +13,45 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
+                // Avatar
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                    .padding()
+                
+                // Info: name, Email, Member since
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name: ")
+                            .bold()
+                        Text("Poopy")
+                    }
+                    .padding()
+                    HStack {
+                        Text("Email: ")
+                            .bold()
+                        Text("Poopy")
+                    }
+                    .padding()
+                    HStack {
+                        Text("Member Since: ")
+                            .bold()
+                        Text("Poopy")
+                    }
+                    .padding()
+                }
+                .padding()
+                
+                // Sign out
+                Button("Log Out") {
+                    viewModel.logout()
+                }
+                .tint(.red)
+                .padding()
+                
+                Spacer()
                 
             }
             .navigationTitle("Profile")
